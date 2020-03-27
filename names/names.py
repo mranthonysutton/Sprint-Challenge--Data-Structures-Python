11cbc0d2ac3f84dbc3705e196bc6157d1da57201
Anthony Sutton <anthony.sutton95@gmail.com>
2020-03-27 10:08:24 -0700

    Completed names stretch

--- names/names.py
+++ names/names.py
@@ -40,3 +40,18 @@ print(f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+stretch_start_time = time.time()
+
+dict_names = {}
+duplicate_result = []
+
+for name in names_1:
+	dict_names[name] = 1
+
+for name in names_2:
+	if name in dict_names:
+		duplicate_result.append(name)
+
+stretch_end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"STRETCH RUNTIME: {stretch_end_time - stretch_start_time} seconds")
